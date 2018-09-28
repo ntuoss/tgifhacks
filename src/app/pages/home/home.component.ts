7d74103ccf99b244d7a36a2d9dd61a5063492859
Clarence Castillo <clarencecastillo@outlook.com>
2018-09-28 15:47:28 +0800

    Move event details to component ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,21 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  event: Event = {
+    subTitle: 'TGIFHacks #86',
+    title: 'Shopee: Developing High Performance Web Apps',
+    date: 'Friday, 28 September 2018',
+    time: '6:30 PM - 8:30 PM Singapore Standard Time',
+    venue: 'NTU LT1 Block NS3 NS3-02-09'
+  };
+
   links: Link[] = [
     {
       title: 'Workshop Live Q&A',
       url: 'https://links.ntuoss.com/ask',
       qrUrl: 'https://links.ntuoss.com/ask.qr'
     },
+    // {
+    //   title: 'Workshop Content',
+    //   url: 'https://links.ntuoss.com/workshop',
+    //   qrUrl: 'https://links.ntuoss.com/workshop.qr'
+    // },
     {
       title: 'Workshop Feedback',
       url: 'https://links.ntuoss.com/feedback',
       qrUrl: 'https://links.ntuoss.com/feedback.qr'
-    },
-    {
-      title: 'Workshop Content',
-      url: 'https://links.ntuoss.com/workshop',
-      qrUrl: 'https://links.ntuoss.com/workshop.qr'
     },
   ];
 
@@ -30,6 +38,14 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+}
+
+interface Event {
+  subTitle: string;
+  title: string;
+  date: string;
+  time: string;
+  venue: string;
 }
 
 export interface Link {
