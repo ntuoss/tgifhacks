c5e91fb1a18648904abaf24e612eb86141982b8e
Clarence Castillo <clarencecastillo@outlook.com>
2018-10-19 17:56:22 +0800

    Hide QnA link

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,11 +16,11 @@ export class HomeComponent implements OnInit {
   };
 
   links: Link[] = [
-    {
-      title: 'Workshop Live Q&A',
-      url: 'https://links.ntuoss.com/ask',
-      qrUrl: 'https://links.ntuoss.com/ask.qr'
-    },
+    // {
+    //   title: 'Workshop Live Q&A',
+    //   url: 'https://links.ntuoss.com/ask',
+    //   qrUrl: 'https://links.ntuoss.com/ask.qr'
+    // },
     {
       title: 'Workshop Content',
       url: 'https://links.ntuoss.com/workshop',
