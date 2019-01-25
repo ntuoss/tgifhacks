6a0c8b87dc6fc699f5d8cb35da0dfdfb743e6107
Clarence Castillo <clarencecastillo@outlook.com>
2019-01-25 18:02:17 +0800

    Update tgif hacks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,24 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   event: Event = {
-    subTitle: 'TGIFHacks #90',
-    title: 'Blockchains & Smart Contracts',
-    date: 'Friday, 2 November 2018',
+    subTitle: 'TGIFHacks #91',
+    title: 'Google: Android Basics',
+    date: 'Friday, 25 January 2019',
     time: '6:30 PM - 8:30 PM Singapore Standard Time',
     venue: 'NTU LT1 Block NS3 NS3-02-09'
   };
 
   links: Link[] = [
-    // {
-    //   title: 'Workshop Live Q&A',
-    //   url: 'https://links.ntuoss.com/ask',
-    //   qrUrl: 'https://links.ntuoss.com/ask.qr'
-    // },
     {
-      title: 'Workshop Content',
-      url: 'https://links.ntuoss.com/workshop',
-      qrUrl: 'https://links.ntuoss.com/workshop.qr'
+      title: 'Workshop Live Q&A',
+      url: 'https://links.ntuoss.com/ask',
+      qrUrl: 'https://links.ntuoss.com/ask.qr'
     },
+    // {
+    //   title: 'Workshop Content',
+    //   url: 'https://links.ntuoss.com/workshop',
+    //   qrUrl: 'https://links.ntuoss.com/workshop.qr'
+    // },
     {
       title: 'Workshop Feedback',
       url: 'https://links.ntuoss.com/feedback',
