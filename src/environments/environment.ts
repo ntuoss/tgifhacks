dda86a2b57a893f69aff274c749b8c5cc483f681
Clarence Castillo <clarencecastillo@outlook.com>
2019-02-07 15:38:32 +0800

    Set workshop content QR to dynamically show if githubUrl is present

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+// tslint:disable:max-line-length
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCFpsDhMyL75TT5RnYIaLDzcAQDnXg_twQ',
+    authDomain: 'hackoss.firebaseapp.com',
+    databaseURL: 'https://hackoss.firebaseio.com',
+    projectId: 'hackoss',
+    storageBucket: 'hackoss.appspot.com',
+    messagingSenderId: '424154730178'
+  }
 };
-
-/*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
