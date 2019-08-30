487ac396e25e289fd0264885eb3ffb87b16a9c3d
Zayn Jarvis <zaynjarvis@gmail.com>
2019-08-30 12:21:29 +0800

    push for update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,63 +2,66 @@ import { Component, OnInit } from '@angular/core';
 import { EventsService } from 'src/app/services/events/events.service';
 import { Event } from 'hackoss';
 
-const WORKSHOP_LIVE_QNA: Link = {
-  title: 'Workshop Live Q&A',
-  url: 'https://links.ntuoss.com/ask',
-  qrUrl: 'https://links.ntuoss.com/ask.qr'
-};
+// const WORKSHOP_LIVE_QNA: Link = {
+//   title: 'Workshop Live Q&A',
+//   url: 'https://links.ntuoss.com/ask',
+//   qrUrl: 'https://links.ntuoss.com/ask.qr'
+// };
 
 const WORKSHOP_CONTENT: Link = {
-  title: 'Workshop Content',
-  url: 'https://links.ntuoss.com/workshop',
-  qrUrl: 'https://links.ntuoss.com/workshop.qr'
+	title: 'Workshop Material',
+	url: 'https://links.ntuoss.com/workshop',
+	qrUrl: 'https://links.ntuoss.com/workshop.qr',
 };
 
 const WORKSHOP_FEEDBACK: Link = {
-  title: 'Workshop Feedback',
-  url: 'https://links.ntuoss.com/feedback',
-  qrUrl: 'https://links.ntuoss.com/feedback.qr'
+	title: 'Workshop Feedback',
+	url: 'https://links.ntuoss.com/feedback',
+	qrUrl: 'https://links.ntuoss.com/feedback.qr',
 };
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+	event: Event;
 
-  event: Event;
+	links: Link[] = [
+		// WORKSHOP_LIVE_QNA,
+		WORKSHOP_FEEDBACK,
+	];
 
-  links: Link[] = [
-    WORKSHOP_LIVE_QNA,
-    WORKSHOP_FEEDBACK
-  ];
+	constructor(private eventsService: EventsService) {}
 
-  constructor(private eventsService: EventsService) { }
+	ngOnInit() {
+		this.getLatestEvent().then(event => {
+			this.event = event;
+			console.log(event);
+			if (event.githubUrl) {
+				this.links.splice(1, 0, WORKSHOP_CONTENT);
+			}
+		});
+	}
 
-  ngOnInit() {
-    this.getLatestEvent().then(event => {
-      this.event = event;
-
-      if (event.githubUrl) {
-        this.links.splice(1, 0, WORKSHOP_CONTENT);
-      }
-    });
-  }
-
-  async getLatestEvent(): Promise<Event> {
-    const events = await this.eventsService.getEvents([{
-      fieldPath: 'status',
-      opStr: '==',
-      value: 'live'
-    }], 1);
-    return events[0];
-  }
-
+	async getLatestEvent(): Promise<Event> {
+		const events = await this.eventsService.getEvents(
+			[
+				{
+					fieldPath: 'status',
+					opStr: '==',
+					value: 'live',
+				},
+			],
+			1,
+		);
+		return events[0];
+	}
 }
 
 export interface Link {
-  title: string;
-  url: string;
-  qrUrl: string;
+	title: string;
+	url: string;
+	qrUrl: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 // tslint:disable:max-line-length
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyCFpsDhMyL75TT5RnYIaLDzcAQDnXg_twQ',
-    authDomain: 'hackoss.firebaseapp.com',
-    databaseURL: 'https://hackoss.firebaseio.com',
-    projectId: 'hackoss',
-    storageBucket: 'hackoss.appspot.com',
-    messagingSenderId: '424154730178'
-  }
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyCFpsDhMyL75TT5RnYIaLDzcAQDnXg_twQ',
+		authDomain: 'hackoss.firebaseapp.com',
+		databaseURL: 'https://hackoss.firebaseio.com',
+		projectId: 'hackoss',
+		storageBucket: 'hackoss.appspot.com',
+		messagingSenderId: '424154730178',
+		appId: '1:424154730178:web:177aee1b25ddabc3',
+	},
 };
