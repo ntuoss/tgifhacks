ce65c498419d2d7dbff1dc2b54ceecee68260705
Zayn Jarvis <zaynjarvis@gmail.com>
2019-09-02 15:02:28 +0800

    roll back to automate version

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { EventsService } from 'src/app/services/events/events.service';
 import { Event } from 'hackoss';
 
-// const WORKSHOP_LIVE_QNA: Link = {
-//   title: 'Workshop Live Q&A',
-//   url: 'https://links.ntuoss.com/ask',
-//   qrUrl: 'https://links.ntuoss.com/ask.qr'
-// };
+const WORKSHOP_LIVE_QNA: Link = {
+	title: 'Workshop Live Q&A',
+	url: 'https://links.ntuoss.com/ask',
+	qrUrl: 'https://links.ntuoss.com/ask.qr',
+};
 
 const WORKSHOP_CONTENT: Link = {
 	title: 'Workshop Material',
@@ -26,40 +26,27 @@ const WORKSHOP_FEEDBACK: Link = {
 	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-	// event: Event;
-	event: any;
+	event: Event;
 
-	links: Link[] = [
-		// WORKSHOP_LIVE_QNA,
-		WORKSHOP_FEEDBACK,
-	];
+	links: Link[] = [WORKSHOP_LIVE_QNA, WORKSHOP_FEEDBACK];
 
 	constructor(private eventsService: EventsService) {}
 
 	ngOnInit() {
 		this.getLatestEvent().then(event => {
-			//
-
-			// Friday, August 30 2019
-			// 6:30 PM - 8:30 PM Singapore Standard Time
-			// Nanyang Technological University Block NS3 NS3-02-09 LT1
-			//   <h1 class="subTitle">TGIFHacks #{{event.tgif}}</h1>
-			// <h1 class="title">{{event.title}}</h1>
-			// <br>
-			// <h1 class="info">{{event.startTime | amDateFormat : 'dddd, MMMM D YYYY'}}</h1>
-			// <h1 class="info">{{event.startTime | amDateFormat : 'h:m A'}} - {{event.endTime | amDateFormat : 'h:m A'}} Singapore Standard Time</h1>
-			// <h1 class="info">{{event.venue.addressLine1}} {{event.venue.addressLine2}}</h1>
-			this.event = {
-				tgif: '98',
-				title: 'Data Science 1/3: Getting into Data Science with Pandas',
-				startTime: new Date('Friday, August 30 2019 6:30 PM'),
-				endTime: new Date('Friday, August 30 2019 8:30 PM'),
-				venue: {
-					addressLine1: 'Nanyang Technological University',
-					addressLine2: 'NS3 NS3-02-09 LT1',
-				},
-			};
+			// this.event = {
+			// 	tgif: '98',
+			// 	title: 'Data Science 1/3: Getting into Data Science with Pandas',
+			// 	startTime: new Date('Friday, August 30 2019 6:30 PM'),
+			// 	endTime: new Date('Friday, August 30 2019 8:30 PM'),
+			// 	venue: {
+			// 		addressLine1: 'Nanyang Technological University',
+			// 		addressLine2: 'NS3 NS3-02-09 LT1',
+			// 	},
+			// };
+			this.event = event;
 			if (event.githubUrl) {
+				console.log(event);
 				this.links.splice(1, 0, WORKSHOP_CONTENT);
 			}
 		});
